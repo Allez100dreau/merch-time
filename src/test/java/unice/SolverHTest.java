3ab5740362e75c11a9cde5d01598b1f36026933f
Ossama <ossama1998@windowslive.com>
2020-11-25 13:14:49 +0100

    Adding a test to SolverH #3

--- src/test/java/unice/SolverHTest.java
+++ src/test/java/unice/SolverHTest.java
@@ -27,12 +27,19 @@ public class SolverHTest {
 
     @Test
     public void solve(){
+        //Test 1
         setUp();
-
-        boolean[] expectedResult = {true, true, false, false};
-
+        boolean[] expectedResult = {true, false, false, true};
         boolean[] actualResult = solverH.solve(instanceMT).getChosenItems() ;
+        assertEquals(Arrays.toString(expectedResult), Arrays.toString(actualResult));
 
+        //Test 2
+        capacity = 40 ;
+        numberOfProducts = 12;
+        weights = Arrays.asList(6,10, 13, 4, 7, 4, 3, 8, 4, 4, 6, 9) ;
+        instanceMT = new InstanceMT(capacity ,numberOfProducts, weights);
+        expectedResult = new boolean[]{true, true, true, true, false, false, false, false, false, false, false, false};
+        actualResult = solverH.solve(instanceMT).getChosenItems() ;
         assertEquals(Arrays.toString(expectedResult), Arrays.toString(actualResult));
     }
 }
