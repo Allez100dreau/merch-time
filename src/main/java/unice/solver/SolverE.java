2e80b1a13243545b0dd821141783353f088eab2d
yasmine <yasmine.ben-fredj@etu.unice.fr>
2020-11-29 00:42:29 +0100

    Réduction de doublon solutionE

--- src/main/java/unice/solver/SolverE.java
+++ src/main/java/unice/solver/SolverE.java
@@ -11,13 +11,12 @@ import java.util.*;
 public class SolverE implements ISolver {
 
     private static final Logger log = LoggerFactory.getLogger(SolverE.class);
-    private List<Integer> localWeight;
 
     /**
      * Cherche toutes les solutions qui répondent au problème de sac
      * @param solutions Les solutions déjà listées
      * @param took une solution primaire (de base tout les éléments sont à FALSE == aucun élément pris)
-     * @param primerSolution la somme de tout les objets déjà pris dans took
+     * @param sum1Solution la somme de tout les objets déjà pris dans took
      * @param weight une instance (liste de poids d'objets) de base il y a tout les objets entrée dans le solverE
      * @param capacity la capacité à atteindre pour être solution
      * @param indice l'indice dans took qui correspond au premier objet de l'instance weight
@@ -25,23 +24,26 @@ public class SolverE implements ISolver {
      */
 
 
-    public List<boolean[]> getSolution(List<boolean[]> solutions, boolean[] took, int primerSolution, List<Integer> weight, int capacity, int indice,boolean restart,List<List<Integer>> combination) {
+    public List<ISolution> getSolution(List<ISolution> solutions, boolean[] took, int sum1Solution, List<Integer> weight, int capacity, int indice,boolean restart) {
         ////////// SI INSTANCE VIDE ////////////
-        if (weight.isEmpty()) {                     // si il n'y a plus d'items
-            return solutions;                       // on retourne les solutions actuelles
+        if (weight.isEmpty() ) {                                     // si il n'y a plus d'items
+            return solutions;                                       // on retourne les solutions actuelles
         }
-        int stockElement = weight.get(0);
+
+        int stockElement = weight.get(0);                           // on stocke le 1er element présent car laliste va etres modifier
+
         ////////// SI INSTANCE EST SOLUTION /////////////////
-        boolean[] soluceBool = Arrays.copyOf(took,took.length);// on copie la solution primaire took
-        int soluce = primerSolution ;               // sommes de tous les objets pris dans took
+        boolean[] soluceBool = Arrays.copyOf(took,took.length);     // on copie la solution primaire took
+        int soluce = sum1Solution ;                               // sommes de tous les objets pris dans took
         for(int e = 0 ; e < weight.size() ; e++){
             soluce+= weight.get(e);                 // on ajoute les poids/prix de tous les objets de l'instance
             soluceBool[indice+e] =  true;           // on set à true à partir de indice (car on estime que l'on prend tout ce qui suit indice)
         }
-        int solve = solver(solutions, soluceBool,soluce, capacity,combination); // on récupère 0, 1 ou 2 en fonction du solver
+
+        SolutionMT solutionMT = new SolutionMT(soluceBool);
+        int solve = solver(solutions, solutionMT,soluce, capacity);
         if (solve == 0) {                           // si 0 alors c'est une nouvelle solution
-            solutions.add(soluceBool);              // on l'ajoute à toutes les solutions
-            combination.add(convert(soluceBool));
+            solutions.add(solutionMT);              // on l'ajoute à toutes les solutions
             return solutions;                       // on retourne les solutions actuelles car ça ne sert à rien de continuer (il n'existe pas d'item "gratuit")
         }
 
@@ -50,38 +52,46 @@ public class SolverE implements ISolver {
             return solutions;                       // inutile de chercher plus
         }
 
+
         ///////// RECHERCHE APPROFONDIE //////////
-        boolean[] nouvelleCombinaisonBool = Arrays.copyOf(took,took.length);   // on crée une liste de la taille de la solution primaire "took"
-        int nouvelleCombinaison = primerSolution + weight.get(0); // la somme des poids de la solution primaire + le nouvel élément
+        boolean[] nouvelleCombinaisonBool = took.clone();   // on crée une liste de la taille de la solution primaire "took"
+        int nouvelleCombinaison = sum1Solution + weight.get(0); // la somme des poids de la solution primaire + le nouvel élément
         nouvelleCombinaisonBool[indice]  =  true;   // on dis que l'on prend l'élément premier
         weight.remove(0);                        // on supprime ce même élément de tout les items car on va traiter toutes les possibilités avec
         List<Integer> instanceSecondaire = new ArrayList<>(weight); // on crée une autre instance de la taille de l'instance de base -1 car on a supprimé le premier élément
         int saveIndice = indice;                    // on mémorise l'indice actuel pour relancer à l'indice suivant
-        solve = solver(solutions,nouvelleCombinaisonBool, nouvelleCombinaison, capacity,combination); // on test si l'ajout du premier élément est une solution
+
+        solutionMT = new SolutionMT(nouvelleCombinaisonBool);
+        solve = solver(solutions,solutionMT, nouvelleCombinaison, capacity); // on test si l'ajout du premier élément est une solution
+
+
         //////// AJOUT DU PREMIER ELEMENT SOLUTION ///////////
         if (solve == 0) {               // 0 signifie que c'est une solution
-            solutions.add(nouvelleCombinaisonBool); // si oui on l'ajoute aux solutions
-            combination.add(convert(nouvelleCombinaisonBool));
+            solutions.add(solutionMT); // si oui on l'ajoute aux solutions
         }
 
         //////// POSSIBILITE DE RAJOUTER DES ELEMENTS ///////////
         else if (solve == 2 && !instanceSecondaire.isEmpty()) { // 2 signifie que l'on est en dessous de la capacité donc on peut rajouter des éléments et si il reste des éléments à ajouter
             int stockSecondaire = 0 ;
-            for (int i = 0; i < instanceSecondaire.size(); i++) { // pour chaque éléments restants
+            for (int i = 0; i < instanceSecondaire.size(); i++) {
+                if (saveIndice > instanceSecondaire.size()) {
+                    continue;
+                }
+
                 stockSecondaire = instanceSecondaire.get(0);
                 int combinaisonSecondaire = nouvelleCombinaison + instanceSecondaire.get(i); // accumulateur de nouvelle combinaison et du nouvel élément
-                boolean[] combinaisonSecondaireBool = Arrays.copyOf(nouvelleCombinaisonBool,nouvelleCombinaisonBool.length); // on copie la solution actuelle
-                combinaisonSecondaireBool[saveIndice+1] = true; // on dit que l'on prend cette item
-                instanceSecondaire.remove(i);   // on le supprime de l'instance secondaire car on va traiter toutes les possibilités avec
-                saveIndice += 1;                // on incrémente l'indice
-                i=-1;                           // on décrémente l'indice de la boucle for pour ne manquer aucun éléments
 
-                solve = solver(solutions,combinaisonSecondaireBool, combinaisonSecondaire, capacity, combination); // on récupère la réponse de solver (0,1 ou 2)
+                boolean[] combinaisonSecondaireBool = nouvelleCombinaisonBool.clone(); // on copie la solution actuelle
+                saveIndice += 1;                // on incrémente l'indice
+                combinaisonSecondaireBool[saveIndice] = true; // on dit que l'on prend cette item
+                instanceSecondaire.remove(i);   // on le supprime de l'instance secondaire car on va traiter toutes les possibilités avec
+                i=-1;                           // on décrémente l'indice de la boucle for pour ne manquer aucun éléments
+                solutionMT = new SolutionMT(combinaisonSecondaireBool);
+                solve = solver(solutions,solutionMT, combinaisonSecondaire, capacity); // on récupère la réponse de solver (0,1 ou 2)
 
                 //////// UNE SOLUTION DE TAILLE 2 //////////
                 if (solve == 0) {               // c'est une solution
-                    solutions.add(combinaisonSecondaireBool); // on l'ajoute
-                    combination.add(convert(combinaisonSecondaireBool));
+                    solutions.add(solutionMT); // on l'ajoute
                     if(!instanceSecondaire.isEmpty() &&  instanceSecondaire.get(0)==stockSecondaire) {
                         instanceSecondaire.remove(0);
                         saveIndice++;
@@ -97,16 +107,16 @@ public class SolverE implements ISolver {
                     continue;                   // on skip aussi
 
                 }
-                /////// ON CHERCHE PLUS LOIN //////
                 List<Integer> newInstance = new ArrayList<>(instanceSecondaire);
-                getSolution(solutions, combinaisonSecondaireBool,combinaisonSecondaire,newInstance,capacity,saveIndice+1,false,combination); // il y a encore de la capacité donc on continue avec la solution, accumulateur, instance actuelle et on va donc ajouter d'autres éléments (à partir de l'indice suivant)
+
+                getSolution(solutions, combinaisonSecondaireBool,combinaisonSecondaire,newInstance,capacity,saveIndice+1,false); // il y a encore de la capacité donc on continue avec la solution, accumulateur, instance actuelle et on va donc ajouter d'autres éléments (à partir de l'indice suivant)
             }
         }
         if(!restart){
             return solutions;
         }
         if(!weight.isEmpty()) {
-            while (weight.get(0) == stockElement) {
+            while (weight.get(0) == stockElement ) {
                 weight.remove(0);
                 indice++;
                 if (weight.isEmpty()) {
@@ -115,7 +125,7 @@ public class SolverE implements ISolver {
             }
         }
         //////// ON RECOMMENCE EN PARTANT DE L'ELEMENT SUIVANT /////////
-        return getSolution(solutions, took, 0, weight, capacity, indice + 1,true,combination); // on recommence à partir de l'indice suivant car toutes les combinaisons possible avec le premier élément de l'instance on été testé
+        return getSolution(solutions, took, 0, weight, capacity, indice + 1,true); // on recommence à partir de l'indice suivant car toutes les combinaisons possible avec le premier élément de l'instance on été testé
     }
 
 
@@ -131,18 +141,16 @@ public class SolverE implements ISolver {
      * 2 : capacité non atteinte
      */
 
-    private int solver(List<boolean[]> solutions, boolean[] combination, int total, int capacity, List<List<Integer>> combinationS) {
+    private int solver(List<ISolution> solutions, ISolution combination, int total, int capacity) {
         ////// CAPACITE NON ATTEINTE /////
         if (capacity > total) {
             return 2;
         }
         ////// CAPACITE ATTEINTE /////
-        List<Integer> solutionsS = this.convert(combination);
 
-        if (capacity == total && !combinationS.contains(solutionsS)) { // solution et elle n'est pas encore listée
-
-            for (int i =0 ; i < solutions.size() ; i++ ){
-                if (Arrays.toString(solutions.get(i)).equals(Arrays.toString(combination)) && combinationS.get(i).containsAll(solutionsS)) {
+        if (capacity == total ) { // solution et elle n'est pas encore listée
+                for (int i =0 ; i < solutions.size() ; i++ ){
+                if (solutions.get(i).equals(combination) ) {
                     return 1;
                 }
             }
@@ -160,18 +168,15 @@ public class SolverE implements ISolver {
     @Override
     public Iterator<ISolution> getIterator(Instance instance) {
         ArrayList<ISolution> solve = new ArrayList<>();
-        List<boolean[]> solutions = new ArrayList<>();
+        List<ISolution> solutions = new ArrayList<>();
         List<Integer> weight = new ArrayList<>(instance.getWeights());
-        this.localWeight = instance.getWeights();
 
-        getSolution(solutions,getEmpty(instance.getNumberOfProducts()), 0, weight, instance.getCapacity(), 0,true,new ArrayList<List<Integer>>());
+        getSolution(solutions,getEmpty(instance.getNumberOfProducts()), 0, weight, instance.getCapacity(), 0,true);
 
-
-
-        for (boolean[] solution : solutions) {
-            ISolution solutionS = new SolutionMT(solution);
+        for (ISolution solution : solutions) {
+            ISolution solutionS = solution;
             solve.add(solutionS);
-            log.info("Les solutions possible : {}", solutionS.getSolution(instance.getWeights()));
+            //log.info("Les solutions possible : {}", solutionS.getSolution(instance.getWeights()));
         }
         return solve.iterator();
     }
@@ -187,28 +192,37 @@ public class SolverE implements ISolver {
      */
     @Override
     public boolean isFeasible(Instance instance, ISolution solution) {
-        List<Integer> rest =  convert(solution.getChosenItems());
-        //On ajoute tous les produits non pris dans la solution au "reste"
-
-        return ( getSolution(new ArrayList<boolean[]>(),getEmpty(instance.getNumberOfProducts()),0,rest,instance.getCapacity(),0,true,new ArrayList<List<Integer>>()).size() > 0);
+        List<Integer> rest =  convert(solution.getChosenItems(),instance.getWeights() );
+        return ( getSolution(new ArrayList<ISolution>(),getEmpty(instance.getNumberOfProducts()),0,rest,instance.getCapacity(),0,true).size() > 0);
     }
 
 
-
-    private boolean[] getEmpty(int numberOfProducts){
-        boolean[] took = new boolean[numberOfProducts];
-        for(int i = 0 ; i < numberOfProducts;i++){
+    /**
+     *
+     * @param number le nombre des false dans le tableau
+     * @return retoune un tableau de "number" Valeur false
+     */
+    public boolean[] getEmpty(int number){
+        boolean[] took = new boolean[number];
+        for(int i = 0 ; i < number;i++){
             took[i] = false ;
         }
         return took;
     }
 
-    private List<Integer> convert(boolean[] tab)
+
+    /**
+     *
+     * @param tab tanleau de boolean qui correspond a une solution
+     * @param localWeight liste des entier qui correspend a tt les produit // les taille des tab et localWeight doivent etres egaux
+     * @return liste des entier choisis ( lorsque à l'index de x dans localWeight il y a true dans tab , il est pris )
+     */
+    public List<Integer> convert(boolean[] tab, List<Integer> localWeight)
     {
         List<Integer> list = new ArrayList<>();
         if (tab.length > 0) {
             for (int i = 0; i < tab.length; i++) {
-                if (tab[i] == true) list.add(this.localWeight.get(i));
+                if (tab[i] == true) list.add(localWeight.get(i));
 
             }
         }
--- src/test/java/unice/TestSolverE.java
+++ src/test/java/unice/TestSolverE.java
@@ -18,17 +18,24 @@ public class TestSolverE {
     {
         //test 1
         solverE = new SolverE();
-        Integer[] s = {1,1,1,1,6};
-        List<Integer> sol1 = Arrays.asList(s);
+        boolean[] s = {true, false, false, true, true, false, false, false, false};
 
-        Integer[] weights = {6,1,1,1,1,2,1,7,5,2,1};
+        Integer[] weights = {6,1,1,1,1,7,5,2,1};
         List<Integer> w = Arrays.asList(weights);
         List<Integer> weight = new ArrayList<>(w);
         Collections.sort(weight);
+        Collections.reverse(weight);
         int capacity = 10;
+        List<ISolution> newSolution = solverE.getSolution(new ArrayList<>(), solverE.getEmpty(weight.size()),0,weight,capacity,0,true);
+        boolean contain = false ;
+        for (ISolution smt : newSolution) {
 
-        List<List<Integer>> newSolution = solverE.getSolution(new ArrayList<>(),new ArrayList<>(),weight,capacity);
-        assertTrue(newSolution.contains(sol1));
+            if (Arrays.toString(smt.getChosenItems()).equals(Arrays.toString(s)))
+            {
+                contain = true;
+            }
+        }
+        assertTrue(contain);
 
 
         //test 2
@@ -38,43 +45,59 @@ public class TestSolverE {
         List<Integer> w2 = Arrays.asList(weights2);
         List<Integer> weight2 = new ArrayList<>(w2);
         Collections.sort(weight2);
-        capacity = 21;
+        Collections.reverse(weight2);
 
-        newSolution = solverE.getSolution(new ArrayList<>(),new ArrayList<>(),weight2,capacity);
+        boolean[] s2 = {true, false, true, false, false, false};
+        newSolution = solverE.getSolution(new ArrayList<>(), solverE.getEmpty(weight2.size()),0,weight2,21,0,true);
         assertEquals( 1,newSolution.size());
+
+        contain = false ;
+        for (ISolution smt : newSolution) {
+
+            if (Arrays.toString(smt.getChosenItems()).equals(Arrays.toString(s2)))
+            {
+                contain = true;
+            }
+        }
+        assertTrue(contain);
 
 
         //test 3
         solverE = new SolverE();
 
-        Integer[] weights3 = {6, 14, 10, 7};
+        Integer[] weights3 = { 14, 10, 7,6};
         List<Integer> w3 = Arrays.asList(weights3);
         List<Integer> weight3 = new ArrayList<>(w3);
-        Collections.sort(weight3);
+
         capacity = 20;
 
-        newSolution = solverE.getSolution(new ArrayList<>(),new ArrayList<>(),weight3,capacity);
-        assertEquals( 1,newSolution.size()); // logiquement 1
+        newSolution = solverE.getSolution(new ArrayList<>(), solverE.getEmpty(weight3.size()),0,weight3,capacity,0,true);
+
+        assertEquals( 0,newSolution.size()); //logiquement 1
 
 
     }
 
     @Test
-    public void getIteratorTest(){
+    public void getIteratorTest() {
         solverE = new SolverE();
 
-        Integer[] weights = {7, 13, 13, 8, 8 ,8};
+        Integer[] weights = {7, 13, 13, 8, 8, 8};
         List<Integer> w = Arrays.asList(weights);
         List<Integer> weight = new ArrayList<>(w);
-        Instance instanceMT = new InstanceMT(21,6,weight);
-        boolean[] sol = {false,true,false,true,false,false};
+        Instance instanceMT = new InstanceMT(21, 6, weight);
+        boolean[] sol = {true, false, true, false, false, false};
 
-        ISolution solution= new SolutionMT(sol);
+        ISolution solution = new SolutionMT(sol);
         List<ISolution> listeSol = new ArrayList<>();
         listeSol.add(solution);
-        assertEquals(Arrays.toString(solverE.getIterator(instanceMT).next().getChosenItems()), Arrays.toString(listeSol.iterator().next().getChosenItems()));
-        assertEquals(Arrays.toString(solverE.getIterator(instanceMT).next().getChosenItems()), Arrays.toString(sol));
 
+        // Si c'est la meme solution l'algo ne retourne qu'une seul
+        Iterator<ISolution> iterator = solverE.getIterator(instanceMT);
+        assertEquals(Arrays.toString(iterator.next().getChosenItems()), Arrays.toString(listeSol.iterator().next().getChosenItems()));
+        assertEquals(iterator.hasNext(), false);
+    }
+/*
         //test 3
         solverE = new SolverE();
 
@@ -82,10 +105,12 @@ public class TestSolverE {
         List<Integer> w3 = Arrays.asList(weights3);
         List<Integer> weight3 = new ArrayList<>(w3);
         instanceMT = new InstanceMT(20,4,weight3);
+        iterator = solverE.getIterator(instanceMT);
 
-        boolean[] sol2 = {true,true,false,false};
+        boolean[] sol2 = {true, false, false, true}; // 14 + 6
+        assertEquals(Arrays.toString(iterator.next().getChosenItems()), Arrays.toString(sol2));
+        assertEquals(iterator.hasNext(),false);
 
-        assertEquals(Arrays.toString(solverE.getIterator(instanceMT).next().getChosenItems()), Arrays.toString(sol2));
     }
 
     @Test
@@ -126,12 +151,13 @@ public class TestSolverE {
 
         //2eme test
         //prix disponnibles
-        Integer[] w1 = {6,1,1,1,1,2,1,7,5,2,1};
+        Integer[] w1 = {7,6,5,2,2,1,1,1,1,1,1};
         weights = Arrays.asList(w1);
         //solution choisi
-        boolean[] s1 = {false,true,true,true,true,true,true,false,false,true,true};
+        boolean[] s1 = {false,true,true,true,true,true,false,false,false,true,false};
         instance = new InstanceMT(10,11,weights);
         solution = new SolutionMT(s1);
+        System.out.print(solverE.isFeasible(instance,solution));
 
         assertFalse(solverE.isFeasible(instance,solution));
 
@@ -155,4 +181,5 @@ public class TestSolverE {
 
         assertFalse(solverE.isFeasible(instance,solution));
     }
-}*/
+*/
+}
